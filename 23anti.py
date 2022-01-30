96936a8b6cb7448300b557157252e8ec43a3a235
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-01-30 05:41:47 -0800

    Update 23anti.py

--- 23anti.py
+++ 23anti.py
@@ -1,1 +1,23 @@
+import random
 
+# DNA randomizer copied and modified from 22atseq.py
+
+dna = ''
+
+for i in range(0, 30):
+    i = random.randint(1,4)
+    if i == 1: dna = dna + 'A'
+    elif i == 2: dna = dna + 'T'
+    elif i == 3: dna = dna + 'C'
+    elif i == 4: dna = dna + 'G'
+
+rev = ''
+
+for i in range(len(dna)-1, -1, -1):
+    if dna[i] == 'A': rev = rev + 'T'
+    elif dna[i] == 'T': rev = rev + 'A'
+    elif dna[i] == 'C': rev = rev + 'G'
+    elif dna[i] == 'G': rev = rev + 'C'
+
+print('DNA Sequence:', dna)
+print('Reverse Complement:', rev)
