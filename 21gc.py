516353e77395087a56c2739c22eda0de481492aa
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-01-30 05:40:50 -0800

    Update 21gc.py

--- 21gc.py
+++ 21gc.py
@@ -1,1 +1,14 @@
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+# Use all three formatting methods
 
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT' # feel free to change
+GC = 0
+
+for i in range(0, len(dna)):
+    if dna[i] == 'G' or dna[i] == 'C':
+        GC = 1 + GC
+
+print('%.2f' % (GC / len(dna)))
+print('{:.2f}'.format(GC / len(dna)))
+print(f'{GC / len(dna):.2f}')
