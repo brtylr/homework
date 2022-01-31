716461a2aa2860d93096653bfa0bb129b1043c4e
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-01-30 17:31:14 -0800

    Update 26gcwin.py

--- 26gcwin.py
+++ 26gcwin.py
@@ -1,1 +1,18 @@
+# Write a program that computes the GC fraction of a DNA sequence in a window
+# Window size is 11 nt
+# Output with 4 significant figures using whichever method you prefer
+# Use nested loops
 
+seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
+w = 11
+GC = 0
+
+for i in range(0, len(seq)-w+1):
+    GC = 0
+    win = seq[i:i+w]
+    print(i, end=' ')
+    print(seq[i:i+w], end=' ')
+    for i in range(0, len(win)):
+        if win[i] == 'G' or win[i] == 'C':
+            GC = 1 + GC
+    print('%.4f' % (GC / w))
