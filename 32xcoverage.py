f4267d071f29852b8aa0bdd1f6dd45926ea4b979
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-02-03 23:17:44 -0800

    Update 32xcoverage.py

--- 32xcoverage.py
+++ 32xcoverage.py
@@ -1,1 +1,6 @@
-
+# Write a program that simulates random read coverage over a chromosome
+# Report min, max, and average coverage
+# Make variables for genome size, read number, read length
+# Input values from the command line
+# Note that you will not sample the ends of a chromosome very well
+# So don't count the first and last parts of a chromsome
