c23ee10e931d49dce388605acaa809457cfb367f
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-03-03 07:29:24 -0800

    Update 31entropy.py

--- 31entropy.py
+++ 31entropy.py
@@ -3,3 +3,24 @@
 # E.g. python3 entropy.py 0.4 0.3 0.2 0.1
 # Put the probabilities into a new list
 # Don't forget to convert them to numbers
+
+"""
+python3 31entropy.py 0.1 0.2 0.3 0.4
+1.846
+"""
+
+import math
+import sys
+
+sum = 0
+prob = []
+
+for i in range(1, len(sys.argv)):
+	prob.append(float(sys.argv[i]))
+
+print(prob)
+
+for i in range(len(prob)):
+	sum -= prob[i] * (math.log2(prob[i]))
+
+print(sum)
