5b4aa8840d16f6ab136e788257a0aa20e6ace195
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-03-03 07:31:27 -0800

    Update 33birthday.py

--- 33birthday.py
+++ 33birthday.py
@@ -1,71 +1,46 @@
-import sys
+# You are probably well aware of the 'birthday paradox'
+# https://en.wikipedia.org/wiki/Birthday_problem
+# Let's try simulating it
 
-def KD(dna):
-	sum = 0
-	for aa in range(len(dna)):
-		if dna[aa] == "R": sum += -4.5
-		elif dna[aa] == "K": sum += -3.9
-		elif dna[aa] == "N": sum += -3.5
-		elif dna[aa] == "D": sum += -3.5
-		elif dna[aa] == "Q": sum += -3.5
-		elif dna[aa] == "E": sum += -3.5
-		elif dna[aa] == "H": sum += -3.2
-		elif dna[aa] == "P": sum += -1.6
-		elif dna[aa] == "Y": sum += -1.3
-		elif dna[aa] == "W": sum += -0.9
-		elif dna[aa] == "S": sum += -0.8
-		elif dna[aa] == "T": sum += -0.7
-		elif dna[aa] == "G": sum += -0.4
-		elif dna[aa] == "A": sum += 1.8
-		elif dna[aa] == "M": sum += 1.9
-		elif dna[aa] == "C": sum += 2.5
-		elif dna[aa] == "F": sum += 2.8
-		elif dna[aa] == "L": sum += 3.8
-		elif dna[aa] == "V": sum += 4.2
-		elif dna[aa] == "I": sum += 4.5
-		else: print("Invalid aa!")
-	return sum/len(dna)
+# You will need a list for the 365 day calendar
+# You will need a set number of people (e.g. 25)
+# During each 'trial' you do the following
+#	Choose a person
+#	Git them a random birthday
+#	Store it in the calendar
+# Once you have stored all birthdays, check to see if any have the same day
+# Do this for many trials to see what the probability of sharing a birthday is
 
-def proline(dna):
-	for aa in range(len(dna)):
-		if dna[aa] == "P":	return False
+import random
+#on = 1
+#bmonth = []
+#bday = []
+#birth = []
+#name = []
+shared = 0
 
-seqname = input("Which sequence would you like to run? (e.g. AT1G75120.1): ")
-seqnamecheck = " "
-toggle1 = 0
-toggle2 = 0
+"""
+python3 33birthday.py
+0.571
+"""
 
-with open(sys.argv[1]) as fp:
-	seq = "0"
-	while seqname != seqnamecheck:
-		line = fp.readline()
-		seqnamecheck = line[1:len(seqname)+1]
-	else:
-		while seq == "0" or seq[len(seq)-2] != "*":
-			seq = seq[0:len(seq)-1]
-			seq += fp.readline()
+#while on == 1:
+#	bmonth.append(int(input("Please enter month of birth in MM format (e.g. 07): ")))
+#	bday.append(int(input("Please enter day of birth in DD format (e.g. 15): ")))
+#	name.append(input("Give this person a name: "))	
+#	birth.append(random.randint(0, 364))
+#	check = input("Would you like to add another person? [Y/n?] ")
+#	if check == "Y" or check == "y":	on = 1
+#	else:	on = 0
 
-print(f'\n{line}\n{seq}')
+cal = [0] * 365
+trials = int(input("How many people would you like to run? "))
 
-for i in range(22):
-	sigpep = seq[i:i+8]
-	if KD(sigpep) > 2.5:
-		print(f'{"Suspected signal peptide "}{sigpep}{" with KD = "}{KD(sigpep):.2f}{" found at position "}{i+1}{"."}')
-		toggle1 = 1
-		break
+for i in range(trials):
+	cal[random.randint(0, 364)] += 1
+#	birth.append(random.randint(0, 364))
+	
+for i in range(len(cal)):
+	if cal[i] > 1:	shared += 1
 
-if toggle1 == 0:	print(f'{"Presence of a signal peptide is unlikely. No average value of KD > 2.5 found for an 8-amino acid length sequence within first 30 amino acids."}')
-
-
-for i in range(30, len(seq)-12):
-	phobicity = seq[i:i+11]
-	if KD(phobicity) > 2.0:
-		if proline(phobicity) == False:	break
-		print(f'{"Suspected hydrophobic region "}{phobicity}{" with KD = "}{KD(phobicity):.2f}{" found at position "}{i+1}{". No prolines are present in region."}')
-		toggle2 = 1
-		break
-
-if toggle2 == 0:	print(f'{"Presence of a hydrophobic region is unlikely. No average value of KD > 2.0 found for an 11-amino acid length sequence past the first 30 amino acids."}')
-
-if toggle1 == 0 or toggle2 == 0:	print(f'\n{"Conditions are NOT met for a trans-membrane protein."}\n')
-else:	print(f'\n{"Conditions HAVE been met for a suspected trans-membrane protein."}\n')
+print(f'{shared/trials:.3f}')
