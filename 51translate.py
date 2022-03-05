e8bfc3a0fd9f0df314d333794218fcf389c28caf
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2022-03-05 01:46:48 -0800

    Create 51translate.py

---
+++ 51translate.py
@@ -0,0 +1,50 @@
+gcode = {
+	'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+	'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+	'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+	'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+	'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+	'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+	'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+	'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+	'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+	'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+	'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+	'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+	'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+	'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+	'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+	'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+}
+
+import sys
+
+file = sys.argv[1]
+seq = ""
+pepsequence = ""
+
+#convert genetic sequence into a single string
+with open(file) as fp:
+	for line in fp.readlines():
+		if line[0] == ">":	continue
+		else:	seq += line[0:len(line)-1].upper()
+
+#extra: detect ATG start, return position, terminate program if no ATG is present
+startposition = 0
+startexists = False
+for i in range(len(seq)-2):
+	if seq[i] == "A" and seq[i+1] == "T" and seq[i+2] == "G":
+		startposition = i
+		startexists = True
+		break	
+
+assert(startexists)
+
+#sequence peptide from startposition using gcode dictionary
+for i in range(startposition, len(seq)-2, 3):
+	if seq[i:i+3] in gcode:
+		pep = gcode[seq[i:i+3]]
+		pepsequence += pep
+		if pep == "*": break
+		
+print(pepsequence)
